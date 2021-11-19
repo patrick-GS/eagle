6a3f347495f34351bd89343619a28ae6e4dcc511
Mustache Beard <87588953+Kingache@users.noreply.github.com>
2021-11-19 21:46:31 +0700

    Update kinghelper.py

--- userbot/modules/kinghelper.py
+++ userbot/modules/kinghelper.py
@@ -11,27 +11,27 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.khelp$")
 async def usit(e):
     await e.edit(
-        f"      ╔════════════╗\n     **__⚡️BANTUAN⚡️__**     \n╚════════════╝ \n"
-        f"**Hai King {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "═⎆ developer  : [King Apis](t.me/PacarFerdilla) \n"
-        "═⎆ Repository : [King-Userbot](https://github.com/apisuserbot/King-Userbot) \n"
-        "═⎆ Instragam  : [Instagram King Apis](Instagram.com/apis_goodboy) \n"
-        "═⎆ Grup Support : [King Userbot Support](https://t.me/KingUserbotSupport)")
+        f"      ╔════════════╗\n     **__✟BANTUAN✟__**     \n╚════════════╝ \n"
+        f"**Hai Jamet {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "═⎆ developer  : [Mustache](t.me/Manusiabajingann) \n"
+        "═⎆ Repository : [Ganesha-Userbot](https://github.com/eagleprojects) \n"
+        "═⎆ Instragam  : [Instagram Mustache](Instagram.com/edxwans) \n"
+        "═⎆ Grup Support : [Ganesha Userbot Support](https://t.me/EagleSupport)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
-        f"      ╔════════════╗\n  **__⚡️DAFTAR VARS⚡️__**     \n╚════════════╝ \n"
-        f"**Disini Daftar Vars Dari King** {DEFAULTUSER} :\n"
-        "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/apisuserbot/King-Userbot/King-Userbot/varshelper.txt)")
+        f"      ╔════════════╗\n  **__✟DAFTAR VARS✟__**     \n╚════════════╝ \n"
+        f"**Disini Daftar Vars Dari Ganesha** {DEFAULTUSER} :\n"
+        "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/eagleprojects/Ganesha/Ganesha/varshelper.txt)")
 
 
 CMD_HELP.update(
     {
         "helper": "**✘ Plugin :** `Helper`\
         \n\n  •  **Perintah :** `.khelp`\
-        \n  •  **Function : **Bantuan Untuk ⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️\
+        \n  •  **Function : **Bantuan Untuk ✟ Ganesha-Userbot ✟\
         \n\n  •  **Perintah :** `.vars`\
         \n  •  **Function : **Melihat Daftar Vars\
     "
