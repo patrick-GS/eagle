271767b1970d574f4eaa31902602bf160a6e6f67
Eagle <92156455+eagleprojects@users.noreply.github.com>
2021-11-20 10:31:44 +0700

    Update db.py

--- userbot/modules/db.py
+++ userbot/modules/db.py
@@ -31,7 +31,7 @@ async def amireallydbs(dbs):
                    f"**Database Mongo db** \n\n"
                    f"**Pengguna :** `{DEFAULTUSER}` \n"
                    f"**Status Database :** `{db}`\n"
-                   f"**Branch :** __Ganesha-Userbot__\n"
+                   f"**Branch :** __Eagle-Userbot__\n"
                    f"**Versi Userbot :** `{BOT_VER}`"
                    "")
 
