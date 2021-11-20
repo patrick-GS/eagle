fc14b448119665e616feabeb2436ea1c499ad2ab
Eagle <92156455+eagleprojects@users.noreply.github.com>
2021-11-20 10:34:09 +0700

    Update kinghelper.py

--- userbot/modules/kinghelper.py
+++ userbot/modules/kinghelper.py
@@ -8,13 +8,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.khelp$")
+@register(outgoing=True, pattern="^.ehelp$")
 async def usit(e):
     await e.edit(
         f"      ╔════════════╗\n     **__✟BANTUAN✟__**     \n╚════════════╝ \n"
         f"**Hai Jamet {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         "═⎆ developer  : [Mustache](t.me/Manusiabajingann) \n"
-        "═⎆ Repository : [Ganesha-Userbot](https://github.com/eagleprojects) \n"
+        "═⎆ Repository : [Eagle-Userbot](https://github.com/eagleprojects) \n"
         "═⎆ Instragam  : [Instagram Mustache](Instagram.com/edxwans) \n"
         "═⎆ Grup Support : [Ganesha Userbot Support](https://t.me/EagleSupport)")
 
@@ -24,7 +24,7 @@ async def var(m):
     await m.edit(
         f"      ╔════════════╗\n  **__✟DAFTAR VARS✟__**     \n╚════════════╝ \n"
         f"**Disini Daftar Vars Dari Ganesha** {DEFAULTUSER} :\n"
-        "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/eagleprojects/Ganesha/Ganesha/varshelper.txt)")
+        "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/eagleprojects/eagle/eagle/varshelper.txt)")
 
 
 CMD_HELP.update(
